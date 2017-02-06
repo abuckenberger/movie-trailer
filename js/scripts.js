be2336c31bb6cd4b6d5d1f9b93401827f632d6c1
abuckenberger <abuckenberger@eagles.ewu.edu>
2017-02-06 14:25:50 -0800

    Set up Javascript
    wrapped video in section tag
    *lettering/fitvids/fittext/mediaelement
    
    media element plugin not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-// Awesome Interaction
+//INTERACTION
+
+// Lettering
 console.log('YO');
 $('h1').lettering();
+
+// Fitvids
+
+$('#video').fitVids ();
+
+//MediaPlayer
+
+$('video').mediaelement();
+
+// FitText
+
+$('h1').fitText ();
